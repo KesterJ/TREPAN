ea561e3463420e26461e6866ba54eb1e806a1419
Kester Jarvis <Kester Jarvis>
2017-08-15 17:45:41 +0100

    More work on m-of-n tests

--- TREPAN-skeleton.py
+++ TREPAN-skeleton.py
@@ -57,7 +57,7 @@ def entropy(labels):
     return ent
 
 
-def info_gain(feature, threshold, samples, labels):
+def binary_info_gain(feature, threshold, samples, labels):
     """
     Takes a feature and a threshold, examples and their
     labels, and find the best feature and breakpoint to split on to maximise
@@ -76,8 +76,32 @@ def info_gain(feature, threshold, samples, labels):
     gain = origent - afterent
     return gain
 
+def mofn_info_gain(mofntest, samples, labels):
+    """
+    Takes an m-of-n test with a set of samples and labels, and calculates the
+    information gain provided by that test.
     
-def make_mofn_tests(besttest, C, samples):
+    Structure of an m-of-n test object:
+    (m, [(feat_1, thresh_1, greater_1)...(feat_n, thresh_n, greater_n)])
+    Where m = number of tests that must be passed (i.e. m in m-of-n)
+    feat_i = the feature of the ith test
+    thresh_i = the threshold of the ith test
+    greater_i = a boolean: if true, value must be >= than threshold to pass
+              the test. If false, it must be < threshold.
+    """
+    #Unpack the tests structure
+    m = mofntest[0]
+    septests = mofntest[1]
+    #List comprehension to generate a boolean index that tells us which samples
+    #passed the test.
+    splittest = np.array([samples[:,septest[0]]>=septest[1] if septest[2] else 
+                 samples[:,septest[0]]<septest[1] for septest in septests])
+    #Now check whether the number of tests passed per sample is higher than m
+    split1 = sum(splittest)>=m
+    split2 = np.invert(split1)
+    ###TODO: Finish this
+    
+def make_mofn_tests(besttest, tests, samples, labels):
     """
     Finds the best m-of-n test, using a beam width of 2.
     
@@ -85,18 +109,55 @@ def make_mofn_tests(besttest, C, samples):
     -NEEDS TO KNOW HOW TO COLLAPSE TESTS WHEN TWO REDUNDANT THINGS ARE
     PRESENT e.g. 2-of {y, z, x, ¬x} -> 1-of {y, z}
     -NEEDS TO KNOW WHICH TESTS WERE ALREADY USED ON THIS BRANCH, AND NOT USE
-    THOSE FEATURES AGAIN
+    THOSE FEATURES AGAIN - CAN BE DONE OUTSIDE FUNCTION BY PASSING SUBSET 
+    OF SAMPLES 
     -NEEDS TO AVOID USING TWO TESTS ON THE SAME LITERAL e.g. x > 0.5 and x > 0.7
-    -NEEDS TO TEST USING BOTH x and ¬x AS THE SEED FOR BEAM SEARCH
     """
-
-def construct_test(features, samples):
+    #Initialise beam with best test and its negation
+    initgain =  binary_info_gain(besttest.feature, besttest.threshold, samples, labels)
+    beam = [(besttest, '<'), (besttest, '>')]
+    beamgains = [initgain, initgain]
+    #Initialise current beam (which will be modified within the loops)
+    currentbeam = beam
+    currentgains = beamgains
+    beamchanged = True
+    #Set up loop to repeat until beam isn't changed
+    while beamchanged:
+        beamchanged = False
+        #Loop over the current best m-of-n tests in beam
+        for item in beam:
+            #Loop over the single-features in candidate tests dict
+            for feature in tests:
+                #Loop over the thresholds for the feature
+                for threshold in tests[feature]:
+                    #Loop over greater than/lesser than tests
+                    for sign in ['<','>']:
+                        #Loop over m+1-of-n+1 and m-of-n+1 tests
+                        for operator in [mplusnplus, nplus]:
+                            #Get info gain and compare it
+                            gain = mofn_info_gain(operator, feature, threshold,
+                                           sign, samples, labels)
+                            #Compare gains
+                            if gain > min(currentgains):
+                                #Replace worst in beam if gain better than worst in beam
+                                currentbeam[np.argmin(currentgains)] = (feature, threshold, sign)
+                                currentgains[np.argmin(currentgains)] = gain
+                                beamchanged = True
+        #Set new tests in beam and associated gains
+        beam = currentbeam
+        beamgains = currentgains
+    #Return the best test in beam
+    return beam(np.argmax(beamgains))
+    
+def construct_test(samples, labels):
     """
-    Takes features and samples, and find the best m-of-n test to split on.
+    Takes samples and labels, and find the best m-of-n test to split on.
     """
-    #make_candidate_tests(features, samples)
-    #bestgain = 0;
-    #for c in C:
-        #if gain(c) > bestgain:
-            #besttest = c
-    #make_mofn_tests(besttest, C, samples)
+    tests = make_candidate_tests(samples, labels)
+    bestgain = 0;
+    for test in tests:
+        if binary_info_gain(test) > bestgain:
+        ###TODO: Write the binary_info_gain call above correctly    
+            bestgain = info_gain(test)
+            besttest = test
+    make_mofn_tests(besttest, tests, samples, labels)
